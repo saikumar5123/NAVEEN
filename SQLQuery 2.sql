015db0b07e5493acb60fd1090e76bbb7def534d1
saikumar5123 <141610421+saikumar5123@users.noreply.github.com>
2023-08-10 13:20:56 +0530

    Add files via upload

---
+++ SQLQuery 2.sql
@@ -0,0 +1,12 @@
+create  table Employee4
+(
+EmpID int,
+EmpName varchar (30),
+designation varchar(20),
+salary int
+);
+select *from Employee2;
+
+insert into employee2
+(EmpId,Empname,designation,salary)
+values(1,'srikanth','manager',200000),(2,'subbareddy','hr',100000),(3,'rajasekhar','teamlead',50000);
